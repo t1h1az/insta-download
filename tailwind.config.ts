7bc3779730b979ce7d1134ea108d47b8f6ba9842
tbeats <t1h1az@googlemail.com>
2025-07-02 21:30:52 +0200

    [feature]  prevent light mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,7 +89,7 @@ const config = {
 export default config;
 
 function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
+  let allColors = flattenColorPalette(theme("dark"));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
