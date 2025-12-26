7af60f1b869e00cc44c079ece17eee88e627cd68
tbeats <t1h1az@googlemail.com>
2025-12-26 19:34:39 +0100

    [style]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,8 @@ import { cn } from "@/lib/utils";
 
 import styles from './styles.module.css'
 import "./globals.css";
+import Link from 'next/link';
+import Image from 'next/image';
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -46,7 +48,7 @@ export default async function LocaleLayout({
       >
                           <ReactQueryProvider>
 <header className={cn(styles.header, "flex w-full items-center px-4 py-2 sm:justify-center md:justify-between")}>
-                {/* <Link href="https://app.aey-studios.com" className="flex md:w-5xl">
+                <Link href="https://app.aey-studios.com" className="flex md:w-5xl">
                   <Image
                     src="/images/aey-logo-white.png"
                     width={200}
@@ -54,8 +56,8 @@ export default async function LocaleLayout({
                     alt="AEY Studios Logo"
                     className="md:w-40 w-60 h-auto"
                   />
-                </Link> */}
-                <h2 className="section__headline">An AEY Studios LLC Product</h2>
+                </Link>
+                <h2 className="section__headline" aria-label='Instagram Video Downloader'>ID</h2>
               </header>
               <main className={"flex h-full justify-center overflow-y-auto px-2 sm:w-full"}>
                 {children}
